9e28ce7166054189bba1cc672c9ff958b5b6f8b0
sahilDussaKestone <sahil.d@kestoneglobal.com>
2024-05-25 18:57:00 +0530

    initial api calling

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-const PORT = process.env.PORT || 5008;
+const PORT = process.env.PORT || 5009;
 const mongoString = process.env.MONGODB_URI;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
+const mongoose = require("mongoose");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+mongoose.set("strictQuery", true);
 mongoose
   .connect(mongoString)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    server.listen(PORT, () => {
+      console.log(`Server Started at localhost:${PORT}`);
     });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json({ limit: "150mb" }));
+app.use(express.urlencoded({ limit: "150mb", extended: true }));
+
+app.get("/allUsers", async (req, res) => {
+  try {
+    return res.status(200).json({ data: "Hello World!" });
+  } catch (ex) {
+    console.log("in error");
+    return res.status(00).json({ data: "Hello World!" });
+  }
+});
